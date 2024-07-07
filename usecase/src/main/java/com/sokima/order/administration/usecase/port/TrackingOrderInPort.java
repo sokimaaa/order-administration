8b631ca89ea3c1754164997c189bf1250d2b1f51
sokimaaa <romandenysov9@gmail.com>
2024-07-07 18:58:07 +0300

    update tracking port

--- usecase/src/main/java/com/sokima/order/administration/usecase/port/TrackingOrderInPort.java
+++ usecase/src/main/java/com/sokima/order/administration/usecase/port/TrackingOrderInPort.java
@@ -1,5 +1,8 @@
 package com.sokima.order.administration.usecase.port;
 
-public interface TrackingOrderInPort {
+import com.sokima.order.administration.usecase.query.in.TrackingQuery;
+import com.sokima.order.administration.usecase.query.out.TrackedOrderResponse;
 
+public interface TrackingOrderInPort {
+    TrackedOrderResponse trackOrder(final TrackingQuery trackingQuery);
 }
