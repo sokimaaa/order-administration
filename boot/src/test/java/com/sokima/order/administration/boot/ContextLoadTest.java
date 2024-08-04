6e9f317892e9dc4e4b0dcf8671bf4e1733760020
sokimaaa <romandenysov9@gmail.com>
2024-08-04 17:50:05 +0300

    add profiles

--- boot/src/test/java/com/sokima/order/administration/boot/ContextLoadTest.java
+++ boot/src/test/java/com/sokima/order/administration/boot/ContextLoadTest.java
@@ -3,7 +3,9 @@ package com.sokima.order.administration.boot;
 import com.sokima.order.administration.boot.test.PostgresContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ContextLoadTest implements PostgresContainer {
 
--- infrastructure/src/main/java/com/sokima/order/administration/infrastructure/driven/persistence/ProductEntity.java
+++ infrastructure/src/main/java/com/sokima/order/administration/infrastructure/driven/persistence/ProductEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "products")
 public class ProductEntity {
 
   @Id
